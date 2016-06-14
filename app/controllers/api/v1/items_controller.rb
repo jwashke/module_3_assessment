c9140116684b4fbce5eb21417805e3f145b1ea91
Josh Washke <jwashke@icloud.com>
2016-06-14 09:48:21 -0600

    Implement delete api/v1/items/:id

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,10 @@ class Api::V1::ItemsController < Api::V1::ApiController
   def show
     respond_with Item.find(params[:id])
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    respond_with(@item, status: 204)
+  end
 end
