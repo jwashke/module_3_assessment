8283e2e5d2f9c59feadae19444f375959c9a4c89
Josh Washke <jwashke@icloud.com>
2016-06-14 11:00:26 -0600

    Store show feature test written

---
+++ spec/features/user_can_view_specific_store_spec.rb
@@ -0,0 +1,38 @@
+# As a user
+# After I have searched a zip code for stores
+# When I click the name of a store
+# Then my current path should be "/stores/:store_id"
+
+require "rails_helper"
+
+RSpec.feature "User can view bestbuy stores by zip" do
+  it "displays the 15 closest stores and and their information" do
+    visit root_path
+    fill_in :search, with: "80202"
+    click_button "Search"
+
+    expect(current_path).to eq("/search")
+    within("#store-2740") do
+      click_link "Best Buy Mobile - Cherry Creek Shopping Center"
+    end
+
+    expect(current_path).to eq("/stores/2740")
+    within(".store-info") do
+      expect(page).to have_content("Best Buy Mobile - Cherry Creek Shopping Center")
+      expect(page).to have_content("3000 East First Ave")
+      expect(page).to have_content("Denver, CO 80206")
+      expect(page).to have_content("Mon: 10am-9pm")
+    end
+  end
+end
+
+#{ }"hoursAmPm": "Mon: 10am-9pm; Tue: 10am-9pm; Wed: 10am-9pm; Thurs: 10am-9pm; Fri: 10am-9pm; Sat: 10am-9pm; Sun: 11am-6pm"
+# I should see the store name, store type and address with city, state and zip
+# I should see an unordered list of the store hours in the following format:
+#   * Mon: 10am-9pm
+#   * Tue: 10am-9pm
+#   * Wed: 10am-9pm
+#   * Thurs: 10am-9pm
+#   * Fri: 10am-9pm
+#   * Sat: 10am-9pm
+#   * Sun: 11am-7pm
