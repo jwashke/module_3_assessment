ded6709e9149874f4c0784fedf06b429918314c3
Josh Washke <jwashke@icloud.com>
2016-06-14 11:09:28 -0600

    Create get_store method in store model and update initialize

--- app/models/store.rb
+++ app/models/store.rb
@@ -7,7 +7,10 @@ class Store
     @phone = store["phone"]
     @store_type = store["storeType"]
     @city = store["city"]
+    @state = store["region"]
+    @zip = store["postalCode"]
     @distance = store["distance"]
+    @hours = format_store_hours(store["hoursAmPm"]) if store["hoursAmPm"]
   end
 
   def self.get_stores(zip_code, distance = 25)
@@ -17,5 +20,17 @@ class Store
       Store.new(store)
     end
     [total, stores]
+  end
+
+  def self.get_store(store_id)
+    response = BestBuyService.new.search_by_store_id(store_id)
+    store = response["stores"]
+    Store.new(store)
+  end
+
+  private
+
+  def format_store_hours(hours_string)
+    hours_string.split("; ")
   end
 end
