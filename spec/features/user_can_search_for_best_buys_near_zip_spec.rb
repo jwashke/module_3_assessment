75383b3fd7dc312d82abe63472cf769a9331eb38
Josh Washke <jwashke@icloud.com>
2016-06-14 10:49:21 -0600

    Implement search index view, search feature test passing

--- spec/features/user_can_search_for_best_buys_near_zip_spec.rb
+++ spec/features/user_can_search_for_best_buys_near_zip_spec.rb
@@ -16,12 +16,13 @@ RSpec.feature "User can view bestbuy stores by zip" do
 
     expect(current_path).to eq("/search")
     expect(page).to have_content("17 Total Stores")
-    expect(page).to have_selector ".store-name", count: 10
-    within("#station-66897") do
-      expect(page).to have_content("UDR")
-      expect(page).to have_content("800 Acoma St Denver CO 80204")
-      expect(page).to have_content("0.3117 miles")
-      expect(page).to have_content("Access Times: 24 hours daily")
+    expect(page).to have_selector ".store-info", count: 15
+    within("#store-2740") do
+      expect(page).to have_content("Best Buy Mobile - Cherry Creek Shopping Center")
+      expect(page).to have_content("Denver")
+      expect(page).to have_content("3.25 miles")
+      expect(page).to have_content("303-270-9189")
+      expect(page).to have_content("Store Type: Mobile")
     end
   end
 end
